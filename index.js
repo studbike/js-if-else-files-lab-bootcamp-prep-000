530494f83c5736007394bab1b54aaa592ceb0101
grant diamond <grant.a.diamond@gmail.com>
2018-03-25 22:17:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var name = "Joe"
 var height = "74"
 
-
+if (name === "grant"){
+  console.log("the name is correct")
+}
+else {
+  console.log("expect joe")
+}
 // Don't worry about this
 module.exports = { name, height
 }
-
